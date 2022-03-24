36e5f5a9e85d57563185b30b31326aa465d085b6
AhmedGMohamed <gamal_aharam3@yahoo.com>
2022-03-24 17:47:08 +0200

    Added automated tests for the image proc. function

---
+++ src/tests/modulesSpec/resizerSpec.ts
@@ -0,0 +1,57 @@
+import fs from "fs";
+import path from "path";
+import {
+  resizerWidthHeight,
+  resizerWidth,
+  resizerHeight
+} from "../../modules/resizer";
+describe("Image processing Module testing suite", () => {
+  it("Should Create a file containing an image after providing all 3 valid arguments.", () => {
+    let errorExists: boolean = false;
+    const filePath = `${path.resolve()}\\cache\\santamonica-500x500.jpg`;
+    resizerWidthHeight("santamonica", 500, 500);
+    fs.access(
+      //Checks if it can read the file, if not, it returns an error
+      filePath,
+      fs.constants.R_OK,
+      async (err: NodeJS.ErrnoException | null) => {
+        if (err) {
+          errorExists = true;
+        } else errorExists = false;
+      }
+    );
+    expect(errorExists).toBe(false);
+  });
+  it("Should Create a file containing an image after providing file name and width only.", () => {
+    let errorExists: boolean = false;
+    const filePath = `${path.resolve()}\\cache\\santamonica-500x_.jpg`;
+    resizerWidth("santamonica", 500);
+    fs.access(
+      //Checks if it can read the file, if not, it returns an error
+      filePath,
+      fs.constants.R_OK,
+      async (err: NodeJS.ErrnoException | null) => {
+        if (err) {
+          errorExists = true;
+        } else errorExists = false;
+      }
+    );
+    expect(errorExists).toBe(false);
+  });
+  it("Should Create a file containing an image after providing file name and height only.", () => {
+    let errorExists: boolean = false;
+    const filePath = `${path.resolve()}\\cache\\santamonica-_x500.jpg`;
+    resizerHeight("santamonica", 500);
+    fs.access(
+      //Checks if it can read the file, if not, it returns an error
+      filePath,
+      fs.constants.R_OK,
+      async (err: NodeJS.ErrnoException | null) => {
+        if (err) {
+          errorExists = true;
+        } else errorExists = false;
+      }
+    );
+    expect(errorExists).toBe(false);
+  });
+});
