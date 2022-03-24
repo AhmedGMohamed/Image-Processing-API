80865e61f13acdbbd59c6d3fe2a1b13f28ac24d4
AhmedGMohamed <gamal_aharam3@yahoo.com>
2022-03-24 17:13:20 +0200

    Added comments to resizer module

--- src/modules/resizer.ts
+++ src/modules/resizer.ts
@@ -2,14 +2,20 @@ import sharp from "sharp";
 import path from "path";
 import { promises as fsPromises } from "fs";
 
-/**Using the sharp library to resize the image using width & height
+/**
+ * @description functions using the sharp library to resize the image using width & height
  * and storing the data in a buffer
- **/
+ * @param {string} fileName
+ * @param {number} imgWidth
+ * @param {number} imgHeight
+ * @return {Promise<void>}
+ */
 async function resizerWidthHeight(
   fileName: string,
   imgWidth: number,
   imgHeight: number
 ): Promise<void> {
+  //process the file using the arguments passed and storing the data in the buffer
   const data: Buffer = await sharp(
     `${path.resolve()}\\src\\images\\${fileName}.jpg`
   )
@@ -21,7 +27,16 @@ async function resizerWidthHeight(
     data
   );
 }
+
+/**
+ * @description function using the sharp library to resize the image using width only
+ * and storing the data in a buffer
+ * @param {string} fileName
+ * @param {number} imgWidth
+ * @return {Promise<void>}
+ */
 async function resizerWidth(fileName: string, imgWidth: number): Promise<void> {
+  //process the file using the arguments passed and storing the data in the buffer
   const data: Buffer = await sharp(
     `${path.resolve()}\\src\\images\\${fileName}.jpg`
   )
@@ -33,10 +48,18 @@ async function resizerWidth(fileName: string, imgWidth: number): Promise<void> {
     data
   );
 }
+/**
+ * @description functions using the sharp library to resize the image using height only
+ * and storing the data in a buffer
+ * @param {string} fileName
+ * @param {number} imgHeight
+ * @return {Promise<void>}
+ */
 async function resizerHeight(
   fileName: string,
   imgHeight: number
 ): Promise<void> {
+  //process the file using the arguments passed and storing the data in the buffer
   const data: Buffer = await sharp(
     `${path.resolve()}\\src\\images\\${fileName}.jpg`
   )
